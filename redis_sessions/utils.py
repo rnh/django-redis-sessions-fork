284f3c78bc6e227106c2a7555b10390c694b04b8
hellysmile <hellysmile@gmail.com>
2013-06-09 12:06:50 +0300

    Remove useless stuff

--- redis_sessions/utils.py
+++ redis_sessions/utils.py
@@ -7,7 +7,7 @@ except ImportError:  # Python 3.*
 from redis_sessions import settings
 
 
-def redis_key(key):
+def add_prefix(key):
     if not settings.SESSION_REDIS_PREFIX:
         return key
     return '%s:%s' % (
@@ -16,20 +16,18 @@ def redis_key(key):
     )
 
 
+def remove_prefix(key):
+    if settings.SESSION_REDIS_PREFIX:
+        key = key.replace(
+            '%s:' % settings.SESSION_REDIS_PREFIX, '', 1
+        )
+    return key
+
+
 def prefix(fn):
     @wraps(fn)
     def wrapped(*args, **kwargs):
         args = list(args)
-        args[0] = redis_key(args[0])
+        args[0] = add_prefix(args[0])
         return fn(*args, **kwargs)
     return wrapped
-
-
-def keys_pattern():
-    if settings.SESSION_REDIS_PREFIX:
-        pattern = '%s:*' % (
-            settings.SESSION_REDIS_PREFIX,
-        )
-    else:
-        pattern = '*'
-    return pattern
